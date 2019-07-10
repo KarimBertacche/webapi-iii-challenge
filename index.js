a4362e931648386c9d76d1870f727d82aebdeb17
Karim B <mattiakarim@live.it>
2019-07-10 13:24:13 +0200

    Chore: initialised project and coded initial boilerplate for the index.js file importing requests routes for the post

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code away!
+const express = require('express');
+const postRoute = require('./posts/postRouter');
+
+const server = express();
+
+server.use(postRoute);
+
+server.listen(4000, () => {
+    console.log('** Server running on port 4K **');
+});
