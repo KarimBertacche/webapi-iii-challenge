61917bd05cf4fe4c4a45ba043c45102f192967b9
Karim B <mattiakarim@live.it>
2019-07-11 13:21:55 +0200

    Chore: added features to allow deployment

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // code away!
 const server = require('./server');
 
-server.listen(4000, () => {
-    console.log('** Server running on port 4K **');
+const port = process.env.PORT || 4000;
+
+server.listen(port, () => {
+    console.log(`/n*** Server running on port ${port} ***/n`);
 });
